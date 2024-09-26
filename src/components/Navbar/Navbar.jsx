0ea9d9c6075d21f39dbc383b59dcaceb31aa624f
Ngoc <ngoc.held9@gmail.com>
2024-09-26 14:09:27 +0900

    Hamburger Menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,55 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
-import logo from '../../assets/logo.png';
-import { FaBars, FaTimes } from 'react-icons/fa'; 
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(prevState => !prevState);
+    setIsMenuOpen((prevState) => !prevState);
   };
 
   return (
-    <nav className="navbar">
+    <header className="navbar">
       <div className="logo">
         <Link to="/">
           <img src={logo} alt="Logo" />
         </Link>
       </div>
-      <div className="menu-toggle" onClick={toggleMenu} aria-label="Toggle menu">
+      <div
+        className="menu-toggle"
+        onClick={toggleMenu}
+        aria-expanded={isMenuOpen}
+        role="button"
+        tabIndex={0}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            toggleMenu();
+          }
+        }}
+      >
         {isMenuOpen ? <FaTimes /> : <FaBars />}
       </div>
-      <ul className={`menu ${isMenuOpen ? 'active' : ''}`}>
-        <li><Link to="#next18">Next18 Proteinについて</Link></li>
-        <li><Link to="#products">商品紹介</Link></li>
-        <li><Link to="#testimonials">お客様の声</Link></li>
-        <li><Link to="#advice">専門家からのアドバイス</Link></li>
-        <li><Link to="/contact">お問い合わせ</Link></li>
+      <ul className={`menu ${isMenuOpen ? "active" : ""}`} onClick={() => setIsMenuOpen(false)}>
+        <li>
+          <Link to="#next18">Next18 Proteinについて</Link>
+        </li>
+        <li>
+          <Link to="#products">商品紹介</Link>
+        </li>
+        <li>
+          <Link to="#testimonials">お客様の声</Link>
+        </li>
+        <li>
+          <Link to="#advice">専門家からのアドバイス</Link>
+        </li>
+        <li>
+          <Link to="/contact">お問い合わせ</Link>
+        </li>
       </ul>
-    </nav>
+    </header>
   );
 };
 
